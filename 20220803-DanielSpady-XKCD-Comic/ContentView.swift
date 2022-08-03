a5553a577d0679c4f560129c39bb162eff431e58
Daniel Spady <danielspady@gmail.com>
2022-08-03 18:18:24 -0400

    Initial Commit

---
+++ 20220803-DanielSpady-XKCD-Comic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20220803-DanielSpady-XKCD-Comic
+//
+//  Created by Daniel Spady on 2022-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20220803-DanielSpady-XKCD-Comic/_0220803_DanielSpady_XKCD_ComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0220803_DanielSpady_XKCD_ComicApp.swift
+//  20220803-DanielSpady-XKCD-Comic
+//
+//  Created by Daniel Spady on 2022-08-03.
+//
+
+import SwiftUI
+
+@main
+struct _0220803_DanielSpady_XKCD_ComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
